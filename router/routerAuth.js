eff63c78908c88beb8f12acb420ca7e7246a7d71
vuongbachdoan <vuongbachdoan@gmail.com>
2022-10-27 00:53:56 +0700

    Add google sign in & store user information at mongoDB

--- router/routerAuth.js
+++ router/routerAuth.js
@@ -1,7 +1,10 @@
 const ControllerAuth = require('../controller/controllerAuth');
-
 const router = require('express').Router();
 
-router.get('/', ControllerAuth.userLogin)
+router.get('/', ControllerAuth.viewLogin)
+router.get('/google', ControllerAuth.loginGoogle)
+router.get('/google/callback', ControllerAuth.redirectGoogle)
+router.get('/success', (req, res) => res.render('Success!'))
+router.get('/fail', (req, res) => res.render('Fail!'))
 
-module.exports = router;
+module.exports = router;              
